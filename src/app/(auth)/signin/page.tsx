5b4f2c75ffd8d55e8532e60a777755c53eeb7f37
hima <19623185@std.stei.itb.ac.id>
2024-12-26 13:47:11 +0700

    feat: implement new authentication page with Google sign-in; migrate folder and video forms to resources directory

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { Button } from "../_components/ui/button";
+import { Button } from "../../_components/ui/button";
 import { FaGoogle } from "react-icons/fa";
 
 export default function AuthPage() {
