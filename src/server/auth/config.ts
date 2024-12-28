6f24c85c0aaccfd7259d0c62df41f751214ebed7
aibrahim185 <163955497+aibrahim185@users.noreply.github.com>
2024-12-28 15:06:43 +0700

    feat: update environment configuration for Google authentication; add Docker setup files (#1)

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,15 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-
 import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
@@ -18,32 +11,21 @@ declare module "next-auth" {
       classid?: number;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authConfig = {
-  providers: [
-    Google,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  providers: [Google],
   adapter: PrismaAdapter(db),
+  events: {
+    async createUser({ user }) {
+      if (user.email === "nerolusi3@gmail.com") {
+        await db.user.update({
+          where: { email: user.email },
+          data: { role: "admin" },
+        });
+      }
+    },
+  },
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
