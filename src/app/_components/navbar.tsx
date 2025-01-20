690ca2d9edd4749aa5c95b903377556de19cb91e
jarv1s <13523089@std.stei.itb.ac.id>
2025-01-20 15:23:38 +0700

    fix: update navbar item layout to use flexbox for better alignment and centering

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -128,7 +128,7 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md border p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "flex size-full select-none items-center justify-center space-y-1 rounded-md border p-3 text-center leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className,
           )}
           {...props}
