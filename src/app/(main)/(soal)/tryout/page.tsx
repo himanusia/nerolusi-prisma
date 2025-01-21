9bcf02c6a94ba95eeae61d4365610515bc77b70d
jarv1s <13523089@std.stei.itb.ac.id>
2025-01-21 22:18:18 +0700

    fix: improve layout and styling of tryout list page for better user experience

--- src/app/(main)/(soal)/tryout/page.tsx
+++ src/app/(main)/(soal)/tryout/page.tsx
@@ -49,7 +49,7 @@ export default function TryoutListPage() {
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl font-bold">Tryout List</h1>
       <p>Select a tryout package to start</p>
-      <div className="flex flex-col rounded-lg border">
+      <div className="flex flex-col gap-4">
         {packages?.map((pkg) => (
           <Button
             key={pkg.id}
@@ -57,9 +57,9 @@ export default function TryoutListPage() {
             onClick={() => {
               router.push(`/tryout/${pkg.id}`);
             }}
-            className="flex size-fit w-full flex-col rounded-none border-b p-3"
+            className="flex size-fit min-h-32 w-full min-w-72 flex-col border bg-slate-300 p-3"
           >
-            <h3 className="font-bold">{pkg.name}</h3>
+            <h3 className="text-2xl font-bold">{pkg.name}</h3>
             {pkg.TOstart && (
               <p
                 className={`${new Date(pkg.TOend) < new Date() ? "hidden" : ""}`}
