41050b5c7ad40c4f20ab1a494f3d7ab1a2a0f1d6
jarv1s <13523089@std.stei.itb.ac.id>
2025-02-03 20:01:16 +0700

    fix: error refresh tryout page bug

--- src/app/(main)/(soal)/tryout/page.tsx
+++ src/app/(main)/(soal)/tryout/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { Button } from "~/app/_components/ui/button";
-import ErrorPage from "~/app/error";
-import LoadingPage from "~/app/loading";
-import { api } from "~/trpc/react";
+import TryoutList from "./tryout-list";
 
 export default function TryoutListPage() {
   const { data: session } = useSession();
-  const router = useRouter();
 
-  if (!session || !session.user) {
-    return <LoadingPage />;
-  }
-
-  const classId = session.user.classid;
+  const classId = session?.user?.classid;
 
   if (!classId) {
     return (
@@ -30,48 +21,5 @@ export default function TryoutListPage() {
     );
   }
 
-  const {
-    data: packages,
-    isLoading,
-    isError,
-  } = api.package.getTryoutPackages.useQuery({ classId });
-
-  return isError ? (
-    <ErrorPage />
-  ) : isLoading ? (
-    <LoadingPage />
-  ) : (
-    <div className="flex flex-col items-center gap-4">
-      <h1 className="text-2xl font-bold">Tryout List</h1>
-      <p>Select a tryout package to start</p>
-      <div className="flex flex-col gap-4">
-        {packages?.map((pkg) => (
-          <Button
-            key={pkg.id}
-            variant="ghost"
-            onClick={() => {
-              router.push(`/tryout/${pkg.id}`);
-            }}
-            className="flex size-fit min-h-32 w-full min-w-72 flex-col border bg-slate-300 p-3"
-          >
-            <h3 className="text-2xl font-bold">{pkg.name}</h3>
-            {pkg.TOstart && (
-              <p
-                className={`${new Date(pkg.TOend) < new Date() ? "hidden" : ""}`}
-              >
-                Start Date: {pkg.TOstart.toLocaleString()}
-              </p>
-            )}
-            {pkg.TOend && (
-              <p
-                className={`${new Date(pkg.TOend) < new Date() ? "hidden" : ""}`}
-              >
-                End Date: {pkg.TOend.toLocaleString()}
-              </p>
-            )}
-          </Button>
-        ))}
-      </div>
-    </div>
-  );
+  return <TryoutList classId={classId} />;
 }
