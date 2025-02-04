a776c4d598190348ce9851dfa925b8d487114c98
jarv1s <13523089@std.stei.itb.ac.id>
2025-02-04 21:22:12 +0700

    fix: correct session end time condition for navigation in quiz page

--- src/app/(main)/(soal)/tryout/[packageId]/[sessionId]/page.tsx
+++ src/app/(main)/(soal)/tryout/[packageId]/[sessionId]/page.tsx
@@ -41,8 +41,7 @@ export default function QuizPage() {
   useEffect(() => {
     if (
       sessionDetails?.endTime &&
-      new Date(sessionDetails?.endTime) < new Date() &&
-      new Date(sessionDetails.package.TOend) >= new Date()
+      new Date(sessionDetails?.endTime) === new Date()
     ) {
       router.push(`/tryout/${packageId}`);
     }
