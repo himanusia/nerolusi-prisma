89a5d3df9ae5e90014a2eb5b48415ec40fd39fda
Albertus Christian Poandy <163734409+albertchriss@users.noreply.github.com>
2025-07-15 23:34:23 +0700

    fix: revisi semuanya (#15)

--- src/app/(main)/rekaman/page.tsx
+++ src/app/(main)/rekaman/page.tsx
@@ -2,27 +2,12 @@ import { SubtestType } from "@prisma/client";
 import { HiMiniVideoCamera } from "react-icons/hi2";
 import { Button } from "~/app/_components/ui/button";
 import DaftarRekaman from "./daftar-rekaman";
+import HeadJenisSubtest from "~/app/_components/head-jenis-subtest";
 
 export default function RekamanPage() {
   return (
     <div className="flex flex-col gap-4">
-      <div className="mt-4 flex items-center rounded-lg bg-gray-200 p-6">
-        <div className="flex size-20 shrink-0 items-center justify-center rounded-full bg-[#2B8057]">
-          <HiMiniVideoCamera className="size-10 text-white" />
-        </div>
-        <div className="ml-4 flex w-full flex-col justify-between gap-4">
-          <h2 className="text-3xl font-bold text-[#2B8057]">
-            Rekaman Live Class
-          </h2>
-          <div className="flex w-full max-w-4xl flex-wrap justify-evenly gap-2">
-            {Object.values(SubtestType).map((type) => (
-              <Button key={type} className="flex-1 text-white">
-                {type.toUpperCase()}
-              </Button>
-            ))}
-          </div>
-        </div>
-      </div>
+      <HeadJenisSubtest title="Rekaman Live Class" type="rekaman" />
       <div className="flex flex-col">
         <h2 className="text-2xl font-bold text-green-600">Daftar Rekaman</h2>
         <p className="text-gray-700">Tonton ulang videonya agar kamu lebih paham!</p>
---
+++ src/app/_components/head-jenis-subtest.tsx
@@ -0,0 +1,59 @@
+import { HiMiniVideoCamera } from "react-icons/hi2";
+import { Button } from "./ui/button";
+import { SubtestType } from "@prisma/client";
+import { GiOpenBook } from "react-icons/gi";
+
+interface HeadJenisSubtestProps {
+  title: string;
+  type: "modul" | "rekaman";
+}
+
+export default function HeadJenisSubtest({
+  title,
+  type,
+}: HeadJenisSubtestProps) {
+  return (
+    <div className="mt-4 flex items-center rounded-lg bg-gray-200 p-6">
+      <div className="hidden w-full items-center sm:flex">
+        <div className="flex size-20 shrink-0 items-center justify-center rounded-full bg-[#2B8057]">
+          {type === "rekaman" ? (
+            <HiMiniVideoCamera className="size-10 text-white" />
+          ) : (
+            <GiOpenBook className="size-10 text-white" />
+          )}
+        </div>
+        <div className="ml-4 flex w-full flex-col justify-between gap-4">
+          <h2 className="font-bold text-[#2B8057] text-3xl">
+            {title}
+          </h2>
+          <div className="flex w-full max-w-4xl flex-wrap justify-evenly gap-2">
+            {Object.values(SubtestType).map((type) => (
+              <Button key={type} className="flex-1 text-xs text-white">
+                {type.toUpperCase()}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full flex-col justify-center gap-4 sm:hidden">
+        <div className="flex items-center w-full justify-center">
+          <div className="flex size-14 shrink-0 items-center justify-center rounded-full bg-[#2B8057]">
+            {type === "rekaman" ? (
+              <HiMiniVideoCamera className="size-6 text-white" />
+            ) : (
+              <GiOpenBook className="size-6 text-white" />
+            )}
+          </div>
+          <h2 className="ml-4 text-xl font-bold text-[#2B8057]">{title}</h2>
+        </div>
+        <div className="flex w-full max-w-4xl flex-wrap justify-evenly gap-2">
+          {Object.values(SubtestType).map((type) => (
+            <Button key={type} className="flex-1 text-xs text-white">
+              {type.toUpperCase()}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
